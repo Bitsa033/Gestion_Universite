7591088e0f489f74ed869cfa23a66a6b1266d349
Bitsa033 <bitsapascal033@gmail.com>
2022-01-14 17:59:19 +0100

    ajouut des methodes de selection et de creation de l'api au format json

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,19 +2,63 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Etudiant;
+use App\Repository\EtudiantRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ApiController extends AbstractController
 {
     /**
      * @Route("/api", name="api")
      */
-    public function index(): Response
+    public function index(EtudiantRepository $etudiantRepository): Response
     {
-        return $this->render('api/index.html.twig', [
-            'controller_name' => 'ApiController',
+        $etudiants=$etudiantRepository->findAll();
+        //on instancie un encoder json
+        $encoders=[ new JsonEncoder()];
+        //on instancie un normaliseur donc un convertisseur
+        $normalizer=[new ObjectNormalizer()];
+        //on prepare la convertion de la collection en tableau php
+        $serializer=new Serializer($normalizer,$encoders);
+        //on convertit la collection en Tableau
+        $contentJson=$serializer->serialize($etudiants, 'json' , [
+            'circular_reference_handler' =>function($object){
+                return $object->getId();
+            }
         ]);
+
+        //dd($contentJson);
+
+        $response=new Response($contentJson);
+        $response->headers->set('Content-Type','application/json');
+        return $response;
+        
     }
+
+    /**
+     * @Route("api/create", name="create")
+     */
+    public function create(Request $request,ManagerRegistry $managerRegistry): Response
+    {
+        $donnee= json_decode($request->getContent());
+        $etudiant = new Etudiant();
+        $etudiant->setNom(strtoupper($donnee->nom));
+        $etudiant->setprenom(strtoupper($donnee->prenom));
+        $etudiant->setprenom(strtoupper($donnee->sexe));
+        $etudiant->setCreatedAt(new \DateTime());
+        $manager=$managerRegistry->getManager();
+        $manager->persist($etudiant);
+        $manager->flush();
+        return new Response('données crées',201);
+        //return $this->render('api/creation.html.twig');
+    }
+
+    
 }
