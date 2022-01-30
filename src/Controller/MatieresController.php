68f9572c84979e614d439c3e60caf6eb57e2f0d6
Bitsa033 <bitsapascal033@gmail.com>
2022-01-30 04:18:36 +0100

    Encore un transfert

--- src/Controller/MatieresController.php
+++ src/Controller/MatieresController.php
@@ -2,12 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Ue;
 use App\Entity\Matiere;
+use App\Repository\UeRepository;
+use App\Repository\NiveauRepository;
+use App\Repository\FiliereRepository;
 use App\Repository\MatiereRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MatieresController extends AbstractController
@@ -105,6 +110,127 @@ class MatieresController extends AbstractController
         $manager->flush();
 
         return $this->redirectToRoute('matieres');
+        
+    }
+
+    /**
+     * @Route("ue_filiere", name="ue_filiere")
+     */
+    function ue_filiere(SessionInterface $session,Request $request,FiliereRepository $filiereRepository, NiveauRepository $niveauRepository){
+        //on cherche l'utilisateur connecté
+        $user= $this->getUser();
+        //si l'utilisateur est n'est pas connecté,
+        // on le redirige vers la page de connexion
+        if (!$user) {
+          return $this->redirectToRoute('app_login');
+        }
+
+        if (!empty($request->request->all())) {
+            $filiere=$filiereRepository->find($request->request->get("filiere"));
+            $niveau=$niveauRepository->find($request->request->get('niveau'));
+            $get_filiere=$session->get('filiere',[]);
+            if (!empty($get_filiere)) {
+              $session->set('filiere',$filiere);
+              $session->set('niveau',$niveau);
+            }
+            //dd($session);
+            $session->set('filiere',$filiere);
+            $session->set('niveau',$niveau);
+            return $this->redirectToRoute('transfert_matiere');
+        }
+        return $this->render('universg/filiere_ue.html.twig',[
+            'filieres'=>$filiereRepository->filieresUser($user),
+            'niveaux'=>$niveauRepository->niveauxUser($user),
+        ]);
+    }
+
+    /**
+     * Affectation des matieres à des filieres et niveaux
+     * @Route("transfert_matiere", name="transfert_matiere")
+     */
+    public function transfert_matiere (SessionInterface $session,FiliereRepository $filiereRepository, NiveauRepository $niveauRepository,MatiereRepository $matiereRepository ,Request $request, ManagerRegistry $end)
+    {
+        //on cherche l'utilisateur connecté
+        $user= $this->getUser();
+        //si l'utilisateur est n'est pas connecté,
+        // on le redirige vers la page de connexion
+        if (!$user) {
+          return $this->redirectToRoute('app_login');
+        }
+        $sessionF=$session->get('filiere',[]);
+        $sessionN=$session->get('niveau',[]);
+        if (!empty($sessionF) && !empty($sessionN) && $request->request->get('ue')) {
+            $matiere=$matiereRepository->find($request->request->get('ue'));    
+            //dd($session->get('filiere'),$session->get('niveau'));
+            $filiere=$filiereRepository->find($sessionF);
+            $niveau=$niveauRepository->find($sessionN);
+            $ue=new Ue();
+            $ue->setFiliere($filiere);
+            $ue->setUser($user);
+            $ue->setNiveau($niveau);
+            $ue->setMatiere($matiere);
+            $ue->setCreatedAt(new \DateTime());
+            $manager = $end->getManager();
+            $manager->persist($ue);
+            $manager->flush();
+            
+            return $this->redirectToRoute('transfert_matiere');
+        }
+        $filieresPasEncoreUe=$filiereRepository->find($sessionF);
+       
+        return $this->render('universg/transfert_matiere.html.twig', [
+            'controller_name' => 'MatieresController',
+            'filieres'=>$filiereRepository->filieresUser($user),
+            'matieres'=>$matiereRepository->matiereUserPasEncoreUe($user,$filieresPasEncoreUe),
+            'niveaux'=>$niveauRepository->niveauxUser($user),
+        ]);
+    }
+
+    /**
+     * Affichage des unites d'enseignement
+     * @Route("ue", name="ue")
+     */
+    public function ue (FiliereRepository $filiereRepository,NiveauRepository $niveauRepository ,Request $request, UeRepository $repo)
+    {
+        //on cherche l'utilisateur connecté
+        $user= $this->getUser();
+        //si l'utilisateur est n'est pas connecté,
+        // on le redirige vers la page de connexion
+        if (!$user) {
+          return $this->redirectToRoute('app_login');
+        }
+
+        //sinon on consulte les données
+        $repostn=$this->getDoctrine()->getRepository(Ue::class)->findAll();
+        $uesUserFiliereNiveau=$repo->uesFiliereNiveau($request->request->get('filiere'),$request->request->get('niveau'));
+        return $this->render('universg/ue.html.twig', [
+            'controller_name' => 'MatieresController',
+            'ues'=>$repostn,
+            'filieres'=>$filiereRepository->filieresUser($user),
+            'niveaux'=>$niveauRepository->niveauxUser($user),
+            'recherche'=>$uesUserFiliereNiveau
+        ]);
+    }
+
+
+    /**
+     * @Route("ue/suppression/{id}", name="suppression_ue")
+     */
+    public function suppression_ue(Ue $ue, ManagerRegistry $end)
+    {
+        //on cherche l'utilisateur connecté
+        $user= $this->getUser();
+        //si l'utilisateur est n'est pas connecté,
+        // on le redirige vers la page de connexion
+        if (!$user) {
+          return $this->redirectToRoute('app_login');
+        }
+        //sinon on supprime les données
+        $manager = $end->getManager();
+        $manager->remove($ue);
+        $manager->flush();
+
+        return $this->redirectToRoute('ue');
         
     }
     
