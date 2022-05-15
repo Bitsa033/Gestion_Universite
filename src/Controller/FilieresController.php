8b44dc32c16f660ed76177393a6e9062048ed89b
bitsapascal <bitsapascal033@gmail.com>
2022-05-16 00:58:25 +0100

    ajout de  plusieurs filieres,classes, et semestres

--- src/Controller/FilieresController.php
+++ src/Controller/FilieresController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 /**
  * @Route("filieres_", name="filieres_")
@@ -17,56 +18,86 @@ class FilieresController extends AbstractController
 {
 
     /**
-     * @Route("nb", name="filiere_nb")
+     * @Route("nb", name="nb")
      */
-    public function new(Request $request)
+    public function nb(SessionInterface $session, Request $request)
     {
-       
+        if (!empty($request->request->get('nb_row'))) {
+            $nb_of_row = $request->request->get('nb_row');
+            $get_nb_row = $session->get('nb_row', []);
+            if (!empty($get_nb_row)) {
+                $session->set('nb_row', $nb_of_row);
+            }
+            $session->set('nb_row', $nb_of_row);
+            //dd($session);
+        }
+        return $this->redirectToRoute('filieres_index');
     }
 
     /**
      * Insertion et affichage des filieres
      * @Route("index", name="index")
      */
-    public function filiere(FiliereRepository $filiereRepository,Request $request, ManagerRegistry $end)
+    public function filiere(SessionInterface $session, FiliereRepository $filiereRepository, Request $request, ManagerRegistry $end)
     {
         //on cherche l'utilisateur connecté
-        $user= $this->getUser();
+        $user = $this->getUser();
         //si l'utilisateur est n'est pas connecté,
         // on le redirige vers la page de connexion
         if (!$user) {
-          return $this->redirectToRoute('app_login');
+            return $this->redirectToRoute('app_login');
         }
-
-        $filiere=$request->request->get('nom_f');
-        $sigle=$request->request->get('sigle');
-        $nb_row=array(1);
-        $input_nb=$request->query->get('nb_row');
-
-        for ($i=0; $i <$input_nb ; $i++) { 
-           $nb_row[$i]=$i;
+        //on recupere la valeur du nb_row stocker dans la session
+        $sessionNb = $session->get('nb_row', []);
+        //on cree un tableau qui permettra de generer plusieurs champs dans le post
+        //en fonction de la valeur de nb_row
+        $nb_row = array(1);
+        //pour chaque valeur du compteur i, on ajoutera un champs de plus en consirerant que 
+        //nb_row par defaut=1
+        for ($i = 0; $i < $sessionNb; $i++) {
+            $nb_row[$i] = $i;
         }
-        
-        if (!empty($request->request->all())) {
-            for ($i=0; $i <$input_nb ; $i++) { 
-
-                $filiere=new Filiere();
-                $filiere->setUser($user);
-                $filiere->setNom(ucfirst('toto'));
-                $filiere->setSigle(strtoupper('toto'));
-                $filiere->setCreatedAt(new \datetime);
-                $manager = $end->getManager();
-                $manager->persist($filiere);
-                $manager->flush();
+        //on cree la methode qui permettra d'enregistrer les infos du post dans la bd
+        function insert_into_db($data, ManagerRegistry $end,$user)
+        {
+            foreach ($data as $key => $value) {
+                $k[] = $key;
+                $v[] = $value;
             }
-           //dd($request);
+            $k = implode(",", $k);
+            $v = implode(",", $v);
+            //echo $v;
+            $filiere = new Filiere();
+            $filiere->setUser($user);
+            $filiere->setNom(ucfirst($v));
+            $filiere->setSigle(strtoupper($data[$key]));
+            $filiere->setCreatedAt(new \datetime);
+            $manager = $end->getManager();
+            $manager->persist($filiere);
+            $manager->flush();
         }
-         
+
+        //si on clic sur le boutton enregistrer et que les champs du post ne sont pas vide
+        if (isset($_POST['enregistrer'])) {
+            $session_nb_row = $session->get('nb_row', []);
+            //dd($session_nb_row);
+            for ($i = 0; $i < $session_nb_row; $i++) {
+                $data = array(
+                    'filiere' => $_POST['filiere' . $i],
+                    'abbr'    => $_POST['abbr' . $i]
+                );
+               
+                insert_into_db($data, $end,$user);
+            }
+
+            // return $this->redirectToRoute('niveaux_index');
+        }
+
         return $this->render('filieres/filieres.html.twig', [
             'nb_rows' => $nb_row,
-            'filieres'=>$filiereRepository->filieresUser($user),
-            'filieresNb'=>$filiereRepository->count([
-                'user'=>$user
+            'filieres' => $filiereRepository->filieresUser($user),
+            'filieresNb' => $filiereRepository->count([
+                'user' => $user
             ]),
         ]);
     }
@@ -75,14 +106,14 @@ class FilieresController extends AbstractController
      * Suppression des filieres
      * @Route("suppression/{id}", name="suppression")
      */
-    public function suppression_filiere (Filiere $filiere, ManagerRegistry $end)
+    public function suppression_filiere(Filiere $filiere, ManagerRegistry $end)
     {
         //on cherche l'utilisateur connecté
-        $user= $this->getUser();
+        $user = $this->getUser();
         //si l'utilisateur est n'est pas connecté,
         // on le redirige vers la page de connexion
         if (!$user) {
-          return $this->redirectToRoute('app_login');
+            return $this->redirectToRoute('app_login');
         }
 
         //sinon on supprime les données
@@ -91,7 +122,5 @@ class FilieresController extends AbstractController
         $manager->flush();
 
         return $this->redirectToRoute('filieres_ajoutEt_liste');
-        
     }
-
 }
--- src/Controller/NiveauxController.php
+++ src/Controller/NiveauxController.php
@@ -9,83 +9,126 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 /**
  * @Route("niveaux_", name="niveaux_")
  */
 class NiveauxController extends AbstractController
 {
-   
     /**
-     * Insertion et affichage des niveaux
+     * @Route("nb", name="nb")
+     */
+    public function nb(SessionInterface $session, Request $request)
+    {
+        if (!empty($request->request->get('nb_row'))) {
+            $nb_of_row = $request->request->get('nb_row');
+            $get_nb_row = $session->get('nb_row', []);
+            if (!empty($get_nb_row)) {
+                $session->set('nb_row', $nb_of_row);
+            }
+            $session->set('nb_row', $nb_of_row);
+            //   dd($session);
+        }
+        return $this->redirectToRoute('niveaux_index');
+    }
+
+    /**
+     * Insertion et affichage des classes
      * @Route("index", name="index")
      */
-    public function ajoutEt_liste (NiveauRepository $niveauRepository,Request $request, ManagerRegistry $end)
+    public function classe(SessionInterface $session, NiveauRepository $niveauRepository, Request $request, ManagerRegistry $end)
     {
         //on cherche l'utilisateur connecté
-        $user= $this->getUser();
+        $user = $this->getUser();
         //si l'utilisateur est n'est pas connecté,
         // on le redirige vers la page de connexion
         if (!$user) {
-          return $this->redirectToRoute('app_login');
+            return $this->redirectToRoute('app_login');
         }
-        //sinon on insert les données
-        $nbniveaux=$niveauRepository->count([
-            'user'=>$user]);
+        //on compte le nbre de classes presentes dans la base de donnees
+        $nbniveaux = $niveauRepository->count([
+            'user' => $user
+        ]);
+
+        //si le nombre de classes est == 0 donc vide, on enregistre 6 classes
         if (empty($nbniveaux)) {
 
-           
-               
-            for ($i=1; $i <7 ; $i++) { 
-                    $niveau=new Niveau();
-                    $niveau->setUser($user);
-                    if ($i==1) {
-                        $niveau->setNom(strtoupper("BTS 1"));
-                    }
-                    elseif ($i==2) {
-                        $niveau->setNom(strtoupper("BTS 2"));
-                    }
-                    elseif ($i==3) {
-                        $niveau->setNom(strtoupper("Licence"));
-                    }
-                    elseif ($i==4) {
-                        $niveau->setNom(strtoupper("Master 1"));
-                    }
-                    elseif ($i==5) {
-                        $niveau->setNom(strtoupper("Master 2"));
-                    }
-                    elseif ($i==6) {
-                        $niveau->setNom(strtoupper("Master 3"));
-                    }
-                   
-                    $niveau->setCreatedAt(new \DateTime());
-                    $manager = $end->getManager();
-                    $manager->persist($niveau);
-                    $manager->flush();
-                }
-            
-            
-            return $this->redirectToRoute('niveaux_index');
-        } 
-
-        if (isset($_POST['boutton_niv'])) {
-            $niv=$request->request->get('niv');
-            $niveau=new Niveau();
+            for ($i = 1; $i < 7; $i++) {
+                $niveau = new Niveau();
                 $niveau->setUser($user);
-                $niveau->setNom(strtoupper($niv));
+                if ($i == 1) {
+                    $niveau->setNom(strtoupper("BTS 1"));
+                } elseif ($i == 2) {
+                    $niveau->setNom(strtoupper("BTS 2"));
+                } elseif ($i == 3) {
+                    $niveau->setNom(strtoupper("Licence"));
+                } elseif ($i == 4) {
+                    $niveau->setNom(strtoupper("Master 1"));
+                } elseif ($i == 5) {
+                    $niveau->setNom(strtoupper("Master 2"));
+                } elseif ($i == 6) {
+                    $niveau->setNom(strtoupper("Master 3"));
+                }
+
                 $niveau->setCreatedAt(new \DateTime());
                 $manager = $end->getManager();
                 $manager->persist($niveau);
                 $manager->flush();
+            }
 
-                return $this->redirectToRoute('niveaux_index');
+            return $this->redirectToRoute('niveaux_index');
         }
-        
+
+        //on recupere la valeur du nb_row stocker dans la session
+        $sessionNb = $session->get('nb_row', []);
+        //on cree un tableau qui permettra de generer plusieurs champs dans le post
+        //en fonction de la valeur de nb_row
+        $nb_row = array(1);
+        //pour chaque valeur du compteur i, on ajoutera un champs de plus en consirerant que 
+        //nb_row par defaut=1
+        for ($i = 0; $i < $sessionNb; $i++) {
+            $nb_row[$i] = $i;
+        }
+        //on cree la methode qui permettra d'enregistrer les infos du post dans la bd
+        function insert_into_db($data, ManagerRegistry $end,$user)
+        {
+            foreach ($data as $key => $value) {
+                $k[] = $key;
+                $v[] =  $value;
+            }
+            $k = implode(",", $k);
+            $v = implode(",", $v);
+            $niveau = new Niveau();
+            // echo $v;
+            $niveau->setUser($user);
+            $niveau->setNom(strtoupper($v));
+            $niveau->setCreatedAt(new \DateTime());
+            $manager = $end->getManager();
+            $manager->persist($niveau);
+            $manager->flush();
+        }
+
+        //si on clic sur le boutton enregistrer et que les champs du post ne sont pas vide
+        if (isset($_POST['enregistrer'])) {
+            $session_nb_row = $session->get('nb_row', []);
+            //dd($session_nb_row);
+            for ($i = 0; $i < $session_nb_row; $i++) {
+                $data = array(
+                    'classe' => $_POST['classe' . $i]
+                );
+
+                insert_into_db($data, $end,$user);
+            }
+
+            // return $this->redirectToRoute('niveaux_index');
+        }
+
         return $this->render('niveaux/niveaux.html.twig', [
-            'controller_name' => 'NiveauxController',
-            'niveaux'=>$niveauRepository->niveauxUser($user),
-            'niveauxNb'=>$niveauRepository->count([
-                'user'=>$user
+            'nb_rows' => $nb_row,
+            'niveaux' => $niveauRepository->niveauxUser($user),
+            'niveauxNb' => $niveauRepository->count([
+                'user' => $user
             ]),
         ]);
     }
@@ -94,23 +137,22 @@ class NiveauxController extends AbstractController
      * Suppression des niveaux
      * @Route("suppression/{id}", name="suppression")
      */
-    public function suppression (Niveau $niveau, ManagerRegistry $end)
+    public function suppression(Niveau $niveau, ManagerRegistry $end)
     {
         //on cherche l'utilisateur connecté
-        $user= $this->getUser();
+        $user = $this->getUser();
         //si l'utilisateur est n'est pas connecté,
         // on le redirige vers la page de connexion
         if (!$user) {
-          return $this->redirectToRoute('app_login');
+            return $this->redirectToRoute('app_login');
         }
 
         //sinon on supprime les données
         $manager = $end->getManager();
         $manager->remove($niveau);
         $manager->flush();
-        
+
         return $this->redirectToRoute('niveaux_ajoutEt_liste');
-        
     }
 
     /**
@@ -119,19 +161,18 @@ class NiveauxController extends AbstractController
     public function infos(Niveau $niveau)
     {
         //on cherche l'utilisateur connecté
-        $user= $this->getUser();
+        $user = $this->getUser();
         //si l'utilisateur est n'est pas connecté,
         // on le redirige vers la page de connexion
         if (!$user) {
-          return $this->redirectToRoute('app_login');
+            return $this->redirectToRoute('app_login');
         }
 
         //sinon on consulte les données
         //affichage d'un niveau particulier
         return $this->render('niveaux/consultation_niveau.html.twig', [
             'controller_name' => 'UniversgController',
-            'niveau'=>$niveau
+            'niveau' => $niveau
         ]);
     }
-    
 }
